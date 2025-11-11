6df79306cc87a500b2589bf789f5670711e492c9
ianconnonUA <ivconnongarzia@mail.austral.edu.ar>
2025-11-10 22:45:53 -0300

    Borre codigo para eliminar NA

--- scripts/01_limpieza.R
+++ scripts/01_limpieza.R
@@ -349,11 +349,11 @@ datos_imputados <- datos_imputados |>
   )) |>
   ungroup()
 
-# --- 3. Eliminación de filas con NAs restantes (Menos Antartida) ---
-datos_imputados <- datos_imputados |>
-  filter(!(Provincia != "ANTARTIDA" & (
-    is.na(Temp) | is.na(DD) | is.na(FF) | is.na(Precipitacion_mm) | is.na(PNM)
-  )))
+# # --- 3. Eliminación de filas con NAs restantes (Menos Antartida) ---
+# datos_imputados <- datos_imputados |>
+#   filter(!(Provincia != "ANTARTIDA" & (
+#     is.na(Temp) | is.na(DD) | is.na(FF) | is.na(Precipitacion_mm) | is.na(PNM)
+#   )))
 
 cat("Imputación finalizada.\n")
 
